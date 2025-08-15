6a1ef14b5dc39058351c9bc90836992770fc9b1a
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-08-16 01:15:04 +0600

    Register Form UI done.

--- src/components/modules/Authentication/RegisterForm.module.tsx
+++ src/components/modules/Authentication/RegisterForm.module.tsx
@@ -16,15 +16,20 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Password from "@/components/ui/password";
 
-const registerSchema = z.object({
-  name: z
-    .string()
-    .min(2, { error: "Name is too short" })
-    .max(50, { error: "Name is too long" }),
-  email: z.email(),
-  password: z.string().min(8, { error: "Password is too short" }),
-  confirmPassword: z.string().min(8, { error: "Password is too short" }),
-});
+const registerSchema = z
+  .object({
+    name: z
+      .string()
+      .min(2, { error: "Name is too short" })
+      .max(50, { error: "Name is too long" }),
+    email: z.email(),
+    password: z.string().min(8, { error: "Password is too short" }),
+    confirmPassword: z.string().min(8, { error: "Password is too short" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    error: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 export function RegisterForm({
   className,
